20445b3b2246298c5c70b5ec1219a1b3b5ed8703
kharenzze <str237@gmail.com>
2017-12-09 18:40:49 +0100

    Base game

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public GameObject target;
+    public float minHeight = 4f;
+    public float heightFactor = 1.5f;
+
+    private float height;
+    private Rigidbody targetRigidBody;
+
+    private void Start() {
+        targetRigidBody = target.GetComponent<Rigidbody>();
+    }
+
+    private void LateUpdate() {
+        Vector3 targetPos = target.transform.position;
+        transform.position = targetPos + Vector3.up * height;
+        height = minHeight * ( 1 + targetRigidBody.velocity.magnitude / heightFactor);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float acceleration = 1;
+    public float turnAcceleration = 1;
+
+    private void FixedUpdate() {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        float forwardMovementRate = Input.GetAxis("Vertical");
+        rb.AddForce(transform.forward * forwardMovementRate * acceleration);
+
+        float rotationMovementRate = Input.GetAxis("Horizontal");
+        rb.AddTorque(Vector3.up * rotationMovementRate * turnAcceleration);
+
+    }
+
+}
